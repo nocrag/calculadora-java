7920d8f086f3849b6719b9efa3932174893eea68
nocrag <joaoggr@hotmail.com>
2018-06-18 17:14:13 -0300

    primeiro commit

---
+++ Scan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira um número inteiro");
+        int numero = input.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par");
+        } else if (numero % 2 != 0)
+        System.out.println("Impar");
+
+    }}
+
+
